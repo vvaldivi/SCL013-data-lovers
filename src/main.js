f263163f17a99b7ecdb33d3446532cb976b26da1
estefania moyano <estefaniamoyano@Airdeestefania.home>
2020-04-28 11:48:49 -0400

    data

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import pokemonData from './../data/pokemon/pokemon.js';
+import pokemonData from './data/pokemon/pokemon.js';
 // import data from './data/lol/lol.js';
-import (filterData) from './../data/pokemon/pokemon.js';
+import {filterData} from './data.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //console.log(example, data);
 
-select.addEventListener('change', ()) => {
-    
-}
+//select.addEventListener('change', ()) => {
+
+//}
+
+let data= pokemonData.pokemon;
+console.log(data);
